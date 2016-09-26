82a3f1511ab4dc35ae216b9aad34211f9122bee1
Leah Ramsier <ler13c@my.fsu.edu>
2016-09-26 00:32:03 -0400

    working on it

--- searchbar.php
+++ searchbar.php
@@ -1,6 +1,6 @@
  <body>
        <div class="mainContainer">  <!-- made this class so that the navbar doesn't go all the way across the page -->
-      <div class="navbar navbar-inverse">
+      <div class="navbar navbar-default">
          
          <ul class="nav navbar-nav"> <!-- All items after the "Rad2" title are formatted in an unordered list -->
             <a class="navbar-brand" href="index.php">Rad2</a>
@@ -11,8 +11,6 @@
                 <a class="dropdown-item" href="#">Accounts</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#">VPN Profiles</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Other</a>
               </div>
             </li>
           <!-- Moved new button-->  
@@ -26,22 +24,19 @@
                      <a class="dropdown-item" href="#">User</a>
                      <div class="dropdown-divider"></div>
                      <a class="dropdown-item" href="#">VPN Profile</a>
-                     <div class="dropdown-divider"></div>
-                     <a class="dropdown-item" href="#">Something Cool</a>
                   <!--</div>-->    
           </li>
 
           <li class="nav-item">
-                <form class="form-inline" action="results.php"> <!-- Beginning of the search box -->
+                <form class="navbar-form" action="results.php"> <!-- Beginning of the search box -->
                   <input name="query" class="form-control" type="text" placeholder="Search">
-                  <button type="submit" class="btn btn-success"> Search </button>
                </form>
           </li>
           
 
-<!--          <li class="nav-item"> <!-- beginning of User Profile dropdown content -->
+          <li class="nav-item"> <!-- beginning of User Profile dropdown content -->
                
-<!--               <div class="dropdown">
+               <div class="dropdown">
                   <button class="dropbtn">Sup User</button>
                   <div class="dropdown-content">
                      <div>
